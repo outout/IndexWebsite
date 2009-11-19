9dd72fa374b0c7fb983a84dcb56a1d0834cd0189
Your Name Here <gangaon@gmail.com>
2009-11-19 15:40:37 +0100

    update file organiser

--- src/assets/php/upload.php
+++ src/assets/php/upload.php
@@ -4,6 +4,6 @@ $tempFile = $_FILES['Filedata']['tmp_name'];
 $fileName = $_FILES['Filedata']['name'];
 $fileSize = $_FILES['Filedata']['size'];
 
-move_uploaded_file($tempFile, "../../user/test./" . $fileName);
+move_uploaded_file($tempFile, "../../user/uploads./" . $fileName);
 
 ?>
